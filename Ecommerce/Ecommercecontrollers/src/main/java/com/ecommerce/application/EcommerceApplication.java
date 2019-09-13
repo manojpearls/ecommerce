31a837ab40f8c503e1c23e1d9416f38fcb948cda
Manoj <Manoj@DESKTOP-0ACQ5A0>
2019-09-14 01:04:33 +0530

    Fixed 404 not found issue

--- Ecommerce/Ecommercecontrollers/src/main/java/com/ecommerce/application/EcommerceApplication.java
+++ Ecommerce/Ecommercecontrollers/src/main/java/com/ecommerce/application/EcommerceApplication.java
@@ -2,12 +2,14 @@ package com.ecommerce.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ecommerce")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
-		  SpringApplication.run(EcommerceApplication.class, args);
+		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
 }
